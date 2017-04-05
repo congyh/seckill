e9e969786339244f032c3eaa6a62b507350ec0fe
congyh <yihao.cong@outlook.com>
2017-04-05 23:39:06 +0800

    [WIP]统一异常处理

---
+++ src/main/java/com/github/congyh/seckill/advice/ExceptionAdvice.java
@@ -0,0 +1,19 @@
+package com.github.congyh.seckill.advice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * 统一异常处理
+ *
+ * @author <a href="mailto:yihao.cong@outlook.com">Cong Yihao</a>
+ */
+@ControllerAdvice
+public class ExceptionAdvice {
+
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionAdvice.class);
+
+//    @ExceptionHandler(value = SeckillException.class)
+//    @Re
+}
