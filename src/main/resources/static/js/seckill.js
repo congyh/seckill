5f4ea5852bba085cba6c204f70437abf48b62833
congyh <yihao.cong@outlook.com>
2017-04-06 11:37:12 +0800

    [WIP]初步完成了用户使用手机号登录的功能

--- src/main/resources/static/js/seckill.js
+++ src/main/resources/static/js/seckill.js
@@ -2,41 +2,53 @@
  * Created by <a href="mailto:yihao.cong@outlook.com">Cong Yihao</a> on 17-4-5.
  */
 
-// js模块化, 可以防止污染global环境, 还可以方便其他模块调用
+// js模块化, 可以防止污染global环境, 还可以方便其他模块化
+    // 在进行页面设计的时候先规划好整个流程是很有帮助的
 var seckill = {
-    detail: {
-        init: function(params) {
-            var userPhone = $.cookie('userPhone');
-            var startTime = params.startTime;
-            var endTime = params.endTime;
-            var productId = params.productId;
+        detail: {
+            init: function (params) {
+                console.log("reach init");
+                // 使用jQuery的cookie插件取出名为userPhone的值
+                var userPhone = $.cookie('userPhone');
+                var startTime = params.startTime;
+                var endTime = params.endTime;
+                var productId = params.productId;
 
-            if (!seckill.validatePhone(userPhone)) {
-                // 钱字号的写法是jQuery的选择器, 也就是能够选择页面上的一个元素
-                var userPhoneModal = $('#userPhoneModal');
-                userPhoneModal.modal({
-                    show: true, // 显示弹出层
-                    backdrop: 'static', // 禁止关闭
-                    keyboard: false // 关闭键盘事件
-                });
-                $('userPhoneBtn').click(function () {
-                    var userPhone = $('#userPhone').val();
-                    if (seckill.validatePhone(userPhone)) {
-                        // TODO 电话需要写入cookie
-                        // 刷新页面
-                        window.location.reload();
-                    }
-                });
+                if (!seckill.validatePhone(userPhone)) {
+                    // 钱字号的写法是jQuery的选择器, 也就是能够选择页面上的一个元素
+                    var userPhoneModal = $('#userPhoneModal');
+                    // 设置模态窗口的属性
+                    userPhoneModal.modal({
+                        show: true, // 显示弹出层
+                        backdrop: 'static', // 禁止点击关闭事件, 也就是窗口没有'x'
+                        keyboard: false // 关闭键盘事件, 防止从键盘esc等关闭窗口
+                    });
+                    $('#userPhoneBtn').click(function () {
+                        // 获得输入框的值
+                        var userPhone = $('#userPhone').val();
+                        if (seckill.validatePhone(userPhone)) {
+                            // 电话号码写入cookie, 设置过期时间, 适用范围等
+                            $.cookie('userPhone', userPhone, {expires: 7, path: '/seckill'})
+                            // 重新加载页面, 会顺带触发重新执行seckill.detail.init方法
+                            window.location.reload();
+                        } else {
+                            // 先隐藏, 填好内容后, 再出现(show), 会有一个动态效果(依靠show中的毫秒延迟数实现)
+                            $('#callbackMsg')
+                                .hide()
+                                .html('<label class="label label-danger">手机号填写错误!</label>')
+                                .show(300);
+                        }
+                    });
+                }
+            }
+        },
+
+        // 验证手机号
+        validatePhone: function (phone) {
+            if (phone && phone.length == 11 && !isNaN(phone)) {
+                return true;
+            } else {
+                return false;
             }
         }
-    },
-
-    // 验证手机号
-    validatePhone: function (phone) {
-        if (phone && phone.length == 11 && !isNaN(phone)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-};
+    };
--- src/main/resources/templates/product-detail.ftl
+++ src/main/resources/templates/product-detail.ftl
@@ -2,20 +2,54 @@
 <html>
 <head>
     <title>秒杀详情页</title>
-    <#include "common-head.ftl">
+<#include "common-head.ftl">
 </head>
 
 <body>
-    <div class="container">
-        <div class="panel">
-            <div class="panel-heading">
-                <h2>${product.name}</h2>
+<div class="container">
+    <div class="panel">
+        <div class="panel-heading">
+            <h2>${product.name}</h2>
+        </div>
+        <div class="panel-body">
+            <h2 class="text-danger">
+            <#--时钟图标-->
+                <span class="glyphicon glyphicon-time"></span>
+            <#--计时面板-->
+                <span class="glyphicon" id="seckill-time"></span>
+            </h2>
+        </div>
+    </div>
+</div>
+<#--模态窗口, 用于记录用户手机号-->
+<div id="userPhoneModal" class="modal fade">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h3 class="modal-title">
+                    <span class="glyphicon glyphicon-phone"></span>
+                    联系电话
+                </h3>
             </div>
-            <div class="panel-body">
-
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-xs-8 clo-xs-offset-2">
+                        <input type="text" name="userPhone" id="userPhone"
+                        placeholder="手机号, 例如18810374312" class="form-control" />
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <#--占位, 用于js返回结果-->
+                <span class="glyphicon" id="callbackMsg"></span>
+                <button type="button" id="userPhoneBtn" class="btn btn-success">
+                    <span class="glyphicon glyphicon-phone"></span>
+                    Submit
+                </button>
             </div>
         </div>
     </div>
+</div>
 </body>
 
 <#include "common-script.ftl">
@@ -23,15 +57,15 @@
 <script src="//cdn.bootcss.com/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
 <script src="//cdn.bootcss.com/jquery.countdown/2.2.0/jquery.countdown.min.js"></script>
 <#--引入自定义的js-->
-<script src="../static/js/seckill.js"></script>
-<script type="javascript">
+<script src="/js/seckill.js"></script>
+<script type="text/javascript">
     // 使用jQuery立即执行
     $(function () {
         // 这里参数传入的是一个对象
         seckill.detail.init({
-            productId: ${product.id},
-            startTime: ${product.startTime?datetime},
-            endTime: ${product.endTime?datetime}
+            productId: ${product.id?replace(',', '')},
+            startTime: '${product.startTime?datetime}',
+            endTime: '${product.endTime?datetime}'
         })
     });
 </script>
