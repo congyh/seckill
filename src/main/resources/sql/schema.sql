6ccb5364dec533b2dc4135c932958bd9dd18fff9
congyh <yihao.cong@outlook.com>
2017-04-07 14:04:11 +0800

    按照mysql规范重新创建了数据库

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -6,17 +6,15 @@ CREATE TABLE seckill_product(
   `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '商品库存id',
   `name` varchar(120) NOT NULL COMMENT '商品名称',
   `number` INT UNSIGNED NOT NULL COMMENT '库存数量',
-  `start_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '秒杀开启时间',
-  `end_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '秒杀结束时间',
-  `create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  PRIMARY KEY (id),
-  KEY idx_start_time (start_time),
-  KEY idx_end_time (end_time),
-  KEY idx_create_time (create_time)
+  `gmt_start` DATETIME NOT NULL COMMENT '秒杀开启时间',
+  `gmt_end` DATETIME NOT NULL COMMENT '秒杀结束时间',
+  `gmt_create` DATETIME NOT NULL DEFAULT current_timestamp COMMENT '创建时间',
+  `gmt_modified` DATETIME NOT NULL DEFAULT current_timestamp COMMENT '修改时间',
+  PRIMARY KEY (id)
 )ENGINE = InnoDB AUTO_INCREMENT = 1000 DEFAULT CHARSET = utf8 COMMENT = '秒杀商品库存表';
 
 # 初始化数据
-INSERT INTO seckill_product(name, number, start_time, end_time)
+INSERT INTO seckill_product(name, number, gmt_start, gmt_end)
 VALUES
   ('1000元秒杀iphone6', 100, '2017-03-29 00:00:00', '2018-03-30 00:00:00'),
   ('500元秒杀ipad2', 200, '2017-03-29 00:00:00', '2018-03-30 00:00:00'),
@@ -28,9 +26,9 @@ CREATE TABLE `seckill_order`(
   `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '订单id',
   `seckill_product_id` BIGINT UNSIGNED NOT NULL COMMENT '秒杀商品id',
   `user_phone` BIGINT UNSIGNED NOT NULL COMMENT '用户手机号码',
-  `order_status` TINYINT UNSIGNED NOT NULL DEFAULT  -1 COMMENT '状态标志: -1, 无效; 0, 成功; 1, 已付款; 2, 已发货; 3, 已收货',
-  `create_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  PRIMARY KEY (seckill_product_id, user_phone), # 联合主键
-  UNIQUE KEY uk_id (id),
-  KEY idx_create_time (create_time)
-) ENGINE = InnoDB AUTO_INCREMENT = 10000 DEFAULT CHARSET = utf8 COMMENT '秒杀成功明细表';
+  `order_status` TINYINT UNSIGNED NOT NULL DEFAULT  0 COMMENT '状态标志 : 0, 默认; 1, 已付款; 2, 已发货; 3, 已收货',
+  `gmt_create` DATETIME NOT NULL DEFAULT current_timestamp COMMENT '创建时间',
+  `gmt_modified` DATETIME NOT NULL DEFAULT current_timestamp COMMENT '修改时间',
+  PRIMARY KEY (id),
+  UNIQUE KEY (seckill_product_id, user_phone) # 联合索引
+) ENGINE = InnoDB AUTO_INCREMENT = 10000 DEFAULT CHARSET = utf8 COMMENT '秒杀订单明细表';
